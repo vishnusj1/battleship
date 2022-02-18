9a481edfd43bf3fb205a2814b41d959cf9ffa3f1
vishnusj <vishnusj12@gmail.com>
2022-02-18 17:30:30 +0530

    Add turn function

--- player.js
+++ player.js
@@ -1,11 +1,24 @@
+const GameBoard = require('./gameboard');
+
 const Player = (type) => {
   let turn = false;
   let fleet = [];
 
   const attack = (pos, board) => board.recieveAttack(pos);
 
+  const randomMove = (board) => {
+    const [row, col] = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    const field = board.arr[row][col];
+    if (field === 'miss' || typeof field !== 'object') {
+      randomMove(board);
+    } else {
+      const pos = { row, col };
+      board.recieveAttack(pos);
+    }
+  };
   return {
     attack,
+    randomMove,
     get type() {
       return type;
     },
@@ -17,4 +30,5 @@ const Player = (type) => {
     },
   };
 };
+
 module.exports = Player;
